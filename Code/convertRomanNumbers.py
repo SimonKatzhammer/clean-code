45436e9d821629181db0a68f33c1b4aaac0b30f9
Simon <simicat@icloud.com>
2025-11-21 12:24:57 +0100

    chapter 0 code. implement the example in python

---
+++ Code/convertRomanNumbers.py
@@ -0,0 +1,89 @@
+# I -> III -> IV -> V -> VI -> IX -> X -> XI -> XIV -> XV -> XVI -> XIX -> XX -> XXI -> XXIV -> XXXIX -> XL
+
+#parts: 
+    # input a number -> convert to roman numeral -> output roman numeral
+    # Input: (positive) integer
+    # Output: string (roman numeral)
+    #E.g. 3331 -> "MMMCCCXXXI"
+
+#reverse:
+# 1 is I
+# 2 is II
+# 3 is III
+# 4 is IV
+# 5 is V
+# 6 is VI
+# 7 is VII
+# 8 is VIII
+# 9 is IX
+# 10 is X
+# 11 is XI
+# 14 is XIV
+# 15 is XV
+# 16 is XVI
+# 19 is XIX
+# 20 is XX
+# 21 is XXI
+# 24 is XXIV
+# 39 is XXXIX
+# 40 is XL
+# 50 is L
+# 100 is C
+# 500 is D
+# 1000 is M
+
+# I is 1
+# V is 5
+# X is 10
+# L is 50
+# C is 100
+# D is 500
+# M is 1000
+
+# 33 -> XXXIII == X + X + X + I + I + I == 10 + 10 + 10 + 1 + 1 + 1
+
+
+
+def int_to_roman(num)-> str:
+    return convertRomanNumbers(num)
+
+def convertRomanNumbers(num: int)-> str:
+    if num <= 0:
+        return "Input must be a positive integer"
+    if num == 1:
+        return "I"
+    if num == 2:
+        return "II"
+    if num == 3:
+        return "III"
+    if num == 4:   
+        return "IV"
+    if num == 5:   
+        return "V"
+    if num == 6:
+        return "VI"
+    if num == 7:
+        return "VII"
+    if num == 8:
+        return "VIII"
+    if num == 9:
+        return "IX"
+    if num == 10:
+        return "X"
+    if num == 11:
+        return "XI"
+    if num == 19:
+        return "XIX"
+    if num == 20:
+        return "XX"
+    if num == 24:
+        return "XXIV"
+    if num == 40:
+        return "XL"
+    if num == 50:
+        return "L"
+    
+    
+
+#
+#
